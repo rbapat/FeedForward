a096088deab07e8d66a26b83eb527e1ec5438c1c
rbapat <rorobapat@gmail.com>
2019-05-26 11:17:00 -0700

    Give me liberty or give me sleep

--- PyFeeder/Feeder.py
+++ PyFeeder/Feeder.py
@@ -3,21 +3,6 @@ from textblob import TextBlob
 import threading
 import socket
 
-
-
-def fetch_data(link):
-	article = Article(link, language = 'en')
-	article.download()
-	article.parse()
-	article.nlp()
-
-	return article.summary.replace('\n', '|').encode()
-
-def send_data(data, sock):
-	iLen =str(len(data)).encode()
-	sock.sendall(iLen)
-	sock.sendall(data)
-
 def main():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_address = ('localhost', 5555)
@@ -29,14 +14,19 @@ def main():
 		except Exception:
 			continue
 
-	lastCmd = ''
-
 	try:
-		
 		while True:
 			data = sock.recv(256)
+			article = Article(data.decode(), language = 'en')
+			article.download()
+			article.parse()
+			article.nlp()
 
-			send_data(fetch_data(data.decode()), sock)
+			summary = article.summary.replace('\n', '|').encode()
+
+			iLen = str(len(summary)).encode()
+			sock.sendall(iLen)
+			sock.sendall(summary)
 
 	finally:
 		sock.close()
